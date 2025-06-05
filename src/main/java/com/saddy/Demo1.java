55f2370c2cb72efc3891e58536d2cf270eea37f6
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2025-06-05 10:55:00 +0530

    initial code added

---
+++ src/main/java/com/saddy/Demo1.java
@@ -0,0 +1,17 @@
+package com.saddy;
+
+public class Demo1 {
+
+    static String name = "Saddam Hossain";
+
+    public static void displayName(){
+        System.out.println("Hello, Saddy!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Demo1 class!");
+        System.out.println("Name: " + name);
+        displayName();
+    }
+
+}
