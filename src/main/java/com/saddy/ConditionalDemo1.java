b504df3fb36e230cb7993acc530d479b3c95d9b5
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2025-06-06 10:10:40 +0530

    java basic code added

---
+++ src/main/java/com/saddy/ConditionalDemo1.java
@@ -0,0 +1,48 @@
+package com.saddy;
+
+public class ConditionalDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the ConditionalDemo1 class!");
+
+        int age = 25; // Example age
+
+        if (age < 18) {
+            System.out.println("You are a minor.");
+        } else if (age >= 18 && age < 65) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a senior citizen.");
+        }
+
+        System.out.println("Age: " + age);
+        demoSwitch(3);
+    }
+
+    public static void demoSwitch(int day) {
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
--- src/main/java/com/saddy/Demo1.java
+++ src/main/java/com/saddy/Demo1.java
@@ -12,6 +12,13 @@ public class Demo1 {
         System.out.println("Welcome to the Demo1 class!");
         System.out.println("Name: " + name);
         displayName();
+        long age = 423423583245345L;
+        System.out.println("Age: " + age);
+        if ((age < 18) && (age > 100)) {
+            System.out.println("This will never be printed.");
+        } else {
+            System.out.println("This will always be printed.");
+        }
     }
 
 }
