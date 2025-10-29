7b836789296481d300c3d9cdcec4af01c9f5b960
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2025-10-29 10:14:43 +0530

    parameter added

--- src/test/java/com/saddy/testngdemo/ParallelTests.java
+++ src/test/java/com/saddy/testngdemo/ParallelTests.java
@@ -8,15 +8,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class ParallelTests {
 
     private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("Before suite started::");
+        System.out.println(System.getProperty("user"));
+        System.out.println(System.getProperty("pwd"));
+        System.out.println(System.getProperty("env"));
+    }
     @Parameters("browser")
     @BeforeMethod
     public void setUp(String browser) {
